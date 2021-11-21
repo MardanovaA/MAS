05fda4f9f1f2d0a86ff71001f66c1b287ba02441
Aigul <mardanova065@gmail.com>
2021-11-21 17:13:51 +0300

    Доделала 1 пункт, сделала отправку сообщения из главной функции

--- lab4/dummyagent.py
+++ lab4/dummyagent.py
@@ -4,43 +4,46 @@ from spade.behaviour import OneShotBehaviour, PeriodicBehaviour
 from spade.message import Message
 from spade.template import Template
 import datetime
-import uuid
 
 class testAgent(Agent):
     class RecvBehav(PeriodicBehaviour):
         async def run(self):
-            # print("RecvBehav running")
-
-            msg = await self.receive(timeout=1)  # wait for a message for 10 seconds
+            msg = await self.receive(timeout=1)
             if msg:
-                print("Message received with content: {}".format(msg.body))
+                # Запись в логи
+                print(f'{msg.to} RECEIVED message with content "{msg.body}" from {msg.sender} at {datetime.datetime.now()}')
                 f = open('logs.txt', 'a')
                 f.write(f'{msg.to} RECEIVED message with content "{msg.body}" from {msg.sender} at {datetime.datetime.now()}\n')
                 f.close()
 
     class InformBehav(PeriodicBehaviour):
         async def run(self):
-            print("InformBehav running")
-            msg = Message(to="MA_agent1@01337.io")  # Instantiate the message
-            msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
-            msg.body = "Hello World"  # Set the message content
-            await self.send(msg)
-            print("Message sent!")
+            if self.get('msg'):
+                # Отправка
+                ms = self.get('msg')
+                msg = Message(to=ms['to'])  # Instantiate the message
+                msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
+                msg.body = ms['message']  # Set the message content
+                await self.send(msg)
+                print(f'{msg.sender} SENDS message with content "{msg.body}" to {msg.to} at {datetime.datetime.now()}')
 
-            f = open('logs.txt', 'a')
-            f.write(f'{msg.sender} SENDS message with content "{msg.body}" to {msg.to} at {datetime.datetime.now()}\n')
-            f.close()
+                # Запись в логи
+                f = open('logs.txt', 'a')
+                f.write(f'{msg.sender} SENDS message with content "{msg.body}" to {msg.to} at {datetime.datetime.now()}\n')
+                f.close()
+
+                # Очищение
+                self.set('msg', None)
 
     async def setup(self):
-        print("Agent started")
+        self.presence.approve_all = 'True'
         start_at = datetime.datetime.now() + datetime.timedelta(seconds=5)
-        InformBehav = self.InformBehav(period=100, start_at=start_at)
-        RecvBehav = self.RecvBehav(period=100, start_at=start_at)
+        InformBehav = self.InformBehav(period=10, start_at=start_at)
+        RecvBehav = self.RecvBehav(period=10, start_at=start_at)
         self.add_behaviour(InformBehav)
         template = Template()
         template.set_metadata("performative", "inform")
         self.add_behaviour(RecvBehav, template)
-        print(self.presence.get_contacts())
 
 
 def createAgent(agent_name, agent_pass):
@@ -52,25 +55,31 @@ def createAgent(agent_name, agent_pass):
 
 def getFullName(agent):
     return agent.name+"@01337.io"
+
+
 if __name__ == "__main__":
-    agent1 = createAgent("MA_agent1@01337.io", "Pass_agent1")
-    # agent2 = createAgent("MA_agent2@01337.io", "Pass_agent2")
-    # agent3 = createAgent("MA_agent3@01337.io", "Pass_agent3")
-    # agent4 = createAgent("MA_agent4@01337.io", "Pass_agent4")
-    # agent5 = createAgent("MA_agent5@01337.io", "Pass_agent5")
-    # agent6 = createAgent("MA_agent6@01337.io", "Pass_agent6")
+    # Создание агентов
+    A1 = createAgent("MA_agent1@01337.io", "Pass_agent1")
+    A2 = createAgent("MA_agent2@01337.io", "Pass_agent2")
+    A3 = createAgent("MA_agent3@01337.io", "Pass_agent3")
+    A4 = createAgent("MA_agent4@01337.io", "Pass_agent4")
+    A5 = createAgent("MA_agent5@01337.io", "Pass_agent5")
+    A6 = createAgent("MA_agent6@01337.io", "Pass_agent6")
 
-    print(getFullName(agent1))
-    # agent1.presence.subscribe("MA_agent2@01337.io")
-    # agent1.presence.subscribe("MA_agent2@01337.io")
+    # Отправка сообщенияя
+    fr = {'message': 'message', 'to': getFullName(A3)}
+    A1.set('msg', fr)
 
-    while agent1.is_alive():
+    # Удаление
+    while A1.is_alive():
         try:
             time.sleep(1)
         except KeyboardInterrupt:
-            # senderagent.stop()
-            agent1.stop()
+            A1.stop()
+            A2.stop()
+            A3.stop()
+            A4.stop()
+            A5.stop()
+            A6.stop()
             break
     print("Agents finished")
-    # dummy = DummyAgent("MA_agent1@01337.io", "Pass_agent1")
-    # dummy = DummyAgent("MA_agent2@01337.io", "Pass_agent2")
